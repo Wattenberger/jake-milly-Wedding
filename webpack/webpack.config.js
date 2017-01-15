dcc934410d33d157f3ebd79e249cc5c0d90d28fa
wattenberger <wattenberger@gmail.com>
2017-01-15 16:20:45 -0500

    fix webpack

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -12,45 +12,47 @@ module.exports = {
       "./src/client.js"
     ],
   },
-    target: "web",
-    output: {
-      path: assetsPath,
-      publicPath: "//" + config.STATIC_HOST + "/static/",
-      filename: "app.min.js",
-      chunkFilename: "[chunkhash].js"
-    },
-    resolve: {
-      extensions: ['', '.js', '.jsx'],
-      modulesDirectories: ['node_modules', srcPath]
-    },
-    module: {
-      loaders: [
-        {
-          test: /\.css$/,
-          loaders: ["style", "css"]
-        },{
-          test: /\.less$/,
-          loaders: [
-            'style-loader', 'css-loader', 'less-loader'
-          ]
-        },{
-          test: /\.js$/,
-          exclude: /node_modules/,
-          loader: 'babel-loader',
-        },{
-          test: /\.vue$/,
-          loader: 'vue',
-        },{
-          test: /.*\.(gif|png|jpe?g|svg)$/i,
-          loaders: [
-            'file?hash=sha512&digest=hex&name=[hash].[ext]',
-            'image-webpack?{progressive: true, optimizationLevel: 7, interlaced: false, pngquant:{quality: "65-90", speed: 4}}',
-          ]
-        },
-      ]
-    ],
-    // noParse: [/node_modules/]
-    noParse: [/ignore/]
+  target: "web",
+  output: {
+    path: assetsPath,
+    publicPath: "//" + config.STATIC_HOST + "/static/",
+    filename: "app.min.js",
+    chunkFilename: "[chunkhash].js"
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+    modulesDirectories: ['node_modules', srcPath]
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loaders: ["style", "css"]
+      },{
+        test: /\.less$/,
+        loaders: [
+          'style-loader', 'css-loader', 'less-loader'
+        ]
+      },{
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },{
+        test: /\.vue$/,
+        loader: 'vue',
+      },{
+        test: /.*\.(gif|png|jpe?g|svg)$/i,
+        loaders: [
+          'file?hash=sha512&digest=hex&name=[hash].[ext]',
+          'image-webpack?{progressive: true, optimizationLevel: 7, interlaced: false, pngquant:{quality: "65-90", speed: 4}}',
+        ]
+      },
+    ]
+  },
+  vue: {
+    loaders: {
+      js: 'babel'
+    }
   },
   plugins: [
     new webpack.NoErrorsPlugin(),
